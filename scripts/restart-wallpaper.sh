adbacec4448c1ea6523e903a16953271e8096408
masshirodev <masshiro.iro.kk@gmail.com>
2025-06-29 15:38:37 -0300

    adding hyde files

--- scripts/restart-wallpaper.sh
+++ scripts/restart-wallpaper.sh
@@ -1,5 +1,5 @@
 killall mpvpaper
 # Monitor 2 - LG
-mpvpaper DP-1 ~/Documents/Backgrounds/bg4.mp4 -o "no-audio --loop-file --panscan=1.0" &>/dev/null &
+mpvpaper DP-1 ~/Documents/Backgrounds/bg6.mp4 -o "no-audio --loop-file --panscan=1.0" &>/dev/null &
 # Monitor 1 - LG Ultrawide
 mpvpaper DP-2 ~/Documents/Backgrounds/nun.png -o "no-audio --loop-file --panscan=1.0" &>/dev/null &
